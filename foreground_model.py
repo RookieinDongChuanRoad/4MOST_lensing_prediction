bdf929191b8d2f34947b8301123c29232254a342
rongfu_localwsl <liurongfu@sjtu.edu.cn>
2024-11-10 20:47:55 +0800

    11.10 basically complete,issues regarding SHMR and magnitude unsolved

--- foreground_model.py
+++ foreground_model.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.interpolate import splrep, splev
 from scipy.integrate import quad
 
+#?================================================================================================
+#? Galaxy model
 #* Stellar mass function 
 class SMF_Driver_ETGs():
     #* Stellar mass function for early type galaxies
@@ -76,3 +78,127 @@ class Redshift_Distribution():
         y_interpolate = [quad(self.PDF, self.z_min, x)[0]*normalization for x in x_interpolate]
         return splev(F, splrep(y_interpolate, x_interpolate))
         
+#*ellipticity
+class q_distribution():
+    def __init__(self):
+        self.alpha = 6.28
+        self.beta = 2.05
+
+    def PDF(self,q):
+        #* The probability distribution of axis ratio q 
+        return q**(self.alpha)*(1-q)**(self.beta)
+
+    def CDF(self,q):
+        #* The cumulaive distribution function
+        normalizaion = 1/quad(self.PDF, 0, 1)[0]
+        x_interpolate = np.linspace(0,1,1000)
+        y_interpolate = np.array([quad(self.PDF,0,x)[0]*normalizaion for x in x_interpolate])
+        return splev(q,splrep(x_interpolate, y_interpolate))
+    
+    def CDF_inv(self,F):
+        #* The inverse of CDF
+        #* We can generate a random number F between [0,1], then input into 
+        #* this CDF_inv, to get a sample q
+        normalization = 1/quad(self.PDF, 0,1)[0]
+        x_interpolate = np.linspace(0,1,1000)
+        y_interpolate = np.array([quad(self.PDF,0,x)[0]*normalizaion for x in x_interpolate])
+        return splev(F,splrep(x_interpolate, y_interpolate))
+
+#* Mass-size relation
+class mass_size_relation():
+    def __init__(self):
+        self.mu_R = 0.99
+        self.beta_R = 0.61
+        self.sigma_R = 0.20
+
+    def mu_logRe(self,logM):
+        return self.mu_R + self.beta_R*(logM - 11.4)
+    
+    def draw_logRe(self,logM):
+        mu = self.mu_logRe(logM)
+        sigma = self.sigma_R
+        return np.random.normal(mu,sigma)
+ 
+#?================================================================================================
+#? Halo model
+
+#* Stellar-to-halo mass relation
+class SHMR():
+    def __init__(self):
+        #* parameter from Zu & Mandelbaum 2015
+        self.logM0 = 12.10
+        self.logM1 = 10.31
+        self.beta = 0.33
+        self.delta = 0.42
+        self.gamma = 1.21
+        
+
+    def mu_logMh(self,logM):
+        return self.logM0 + self.beta*(logM - self.logM1) + (10**(logM - self.logM1))**self.delta / (1 + (10**(logM - self.logM1))**(-self.gamma)) - 0.5
+
+#* inner mass density slope
+class inner_slope():
+    #* here we only need the scatter, then we can draw the slope from a normal distribution, given the scatter
+    def __init__(self):
+        self.sigma = 0.2
+
+    def CDF_inv(self,mu_gamma, F):
+        #* The inverse of CDF
+        #* We can generate a random number F between [0,1], then input into 
+        #* this CDF_inv, to get a sample gamma
+        from scipy.stats import norm
+        return norm.ppf(F,loc=mu_gamma, scale=self.sigma)
+    
+#* concentration-mass relation
+class concentration_mass_relation():
+    def __init__(self):
+        self.model = 'Klypin16_m'
+        self.sigma = 0.1
+
+    def mu_logc(self,logM):
+        from colossus.halo import concentration
+        from colossus.cosmology import cosmology
+        cosmology.setCosmology('planck13')
+        return np.log10(concentration.concentration(10**logM, 'vir', 0.1, model=self.model))
+    
+    def CDF_inv(self,logM, F):
+        #* The inverse of CDF
+        #* We can generate a random number F between [0,1], then input into 
+        #* this CDF_inv, to get a sample c
+        from scipy.stats import norm
+        return norm.ppf(F,loc=self.mu_logc(logM), scale=self.sigma)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
